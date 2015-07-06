ebe17cce1fa5fdc93b9ab633a3ba603176a15228
DriftWeb <ann@grontmij.dk>
2015-07-06 21:34:32 +0200

    test

--- js/controllers/ListCtrl.js
+++ js/controllers/ListCtrl.js
@@ -1,33 +1,31 @@
-﻿(function () {
-    angular.module('onsiteApp').controller('ListCtrl', function ($scope) {
-        $scope.list = [
-            {
-                friendlyId: 1,
-                title: "Title 1"
-            },
-            {
-                friendlyId: 2,
-                title: "Title 2"
-            },
-            {
-                friendlyId: 3,
-                title: "Title 3"
-            },
-            {
-                friendlyId: 4,
-                title: "Title 4"
-            },
-            {
-                friendlyId: 5,
-                title: "Title 5"
-            }
-        ];
+﻿angular.module('onsiteApp').controller('listCtrl', function ($scope) {
+    $scope.list = [
+        {
+            friendlyId: 1,
+            title: "Title 1"
+        },
+        {
+            friendlyId: 2,
+            title: "Title 2"
+        },
+        {
+            friendlyId: 3,
+            title: "Title 3"
+        },
+        {
+            friendlyId: 4,
+            title: "Title 4"
+        },
+        {
+            friendlyId: 5,
+            title: "Title 5"
+        }
+    ];
 
-        //$scope.leftSlide = function (item) {
-        //    item.open = true;
-        //};
-        //$scope.rightSlide = function (item) {
-        //    item.open = false;
-        //};
-    });
-})();
+    $scope.leftSlide = function (item) {
+        item.open = true;
+    };
+    $scope.rightSlide = function (item) {
+        item.open = false;
+    };
+});
