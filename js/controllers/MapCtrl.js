2412a6bc476a3e3e987b5079a0df75de48e10a86
Lars <lss@grontmij.dk>
2015-07-06 14:17:55 +0200

    map change

--- js/controllers/MapCtrl.js
+++ js/controllers/MapCtrl.js
@@ -1,5 +1,30 @@
 ﻿(function () {
     angular.module('onsiteApp').controller('MapCtrl', function () {
-        
+
+        initOl3Map();
+
+       
     });
+
+    var initOl3Map = function () {
+        var map = new ol.Map({
+            layers: [
+                new ol.layer.Tile({
+                    source: new ol.source.OSM()
+                })
+            ],
+            controls: ol.control.defaults({
+                attributionOptions: ({
+                    collapsible: false
+                })
+            }),
+            target: 'map',
+            view: new ol.View({
+                center: [0, 0],
+                zoom: 2
+            })
+        });
+
+    }
+
 })();
