d1486fee34b866bb69bb500d5b189c27c9c74d73
creodeusoliver <omoeltoft@gmail.com>
2015-07-06 15:26:32 +0200

    Swipe

--- js/controllers/HomeCtrl.js
+++ js/controllers/HomeCtrl.js
@@ -1,5 +1,10 @@
 ﻿(function () {
     angular.module('onsiteApp').controller('HomeCtrl', function () {
 
+        $scope.swipeRight = function () {
+            $scope.direction = 'right';
+            alert("Swipe right");
+        };
+
     });
 })();
--- js/controllers/ListCtrl.js
+++ js/controllers/ListCtrl.js
@@ -1,10 +1,4 @@
 ﻿(function () {
     angular.module('onsiteApp').controller('ListCtrl', function () {
-
-        $scope.swipeRight = function () {
-            $scope.direction = 'right';
-            alert("Swipe right");
-        };
-
     });
 })();
