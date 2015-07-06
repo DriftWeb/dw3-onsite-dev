f068947d8632bab321433726118a287662723042
DriftWeb <ann@grontmij.dk>
2015-07-06 13:41:00 +0200

    Opsætning

---
+++ js/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+function jsonp_callback(data) {
+    // returning from async callbacks is (generally) meaningless
+    console.log(data.found);
+}
+
+// Declare app level module which depends on filters, and services
+var app = angular.module('onsiteApp',['ngRoute'])
+.config(['$routeProvider', function($routeProvider) {
+        $routeProvider.when('/', {templateUrl: 'partials/homeView.html', controller: 'HomeCtrl'});
+        $routeProvider.otherwise({redirectTo: '/'});
+  }]);
