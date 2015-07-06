66d76bc47afff87c084172b416bd28f8bc91f362
creodeusoliver <omoeltoft@gmail.com>
2015-07-06 15:18:36 +0200

    Swipe test

--- js/controllers/ListCtrl.js
+++ js/controllers/ListCtrl.js
@@ -1,5 +1,10 @@
 ﻿(function () {
     angular.module('onsiteApp').controller('ListCtrl', function () {
 
+        $scope.swipeRight = function () {
+            $scope.direction = 'right';
+            alert("Swipe right");
+        };
+
     });
 })();
