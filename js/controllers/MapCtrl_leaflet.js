106879bc20afe322b203c8e16ae5f08a1679d379
Lars <lss@grontmij.dk>
2015-07-07 14:56:38 +0200

    leaflet libs

--- js/controllers/MapCtrl_leaflet.js
+++ js/controllers/MapCtrl_leaflet.js
@@ -1,6 +1,31 @@
 ﻿(function () {
     angular.module('onsiteApp').controller('MapCtrl_Leaflet', function ($scope, $rootScope, olData, olHelpers) {
         
+        //fortsæt med at vis kort fra kortforsyningen
+      
+        
+        angular.extend($scope, {
+            defaults: {
+                tileLayerOptions: {
+                    attribution: "Driftweb 3 - Grontmij"
+                },
+                tileLayer: "http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png",
+                maxZoom: 14,
+                path: {
+                    weight: 10,
+                    color: '#800000',
+                    opacity: 1
+                }
+            }
+        });
+        angular.extend($scope, {
+            attribution: "Driftweb 3 - Grontmij",
+            center: {
+                lat: 51.505,
+                lng: -0.09,
+                zoom: 8
+            }
+        });
     });
 
 
